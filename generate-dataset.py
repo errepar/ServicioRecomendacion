6638cc317afc7ae81b13adfbb39c49e06908edaf
Francisco <francisco.serrano372@gmail.com>
2019-01-29 15:16:09 -0300

    Adds intermediate dataset generation

--- generate-dataset.py
+++ generate-dataset.py
@@ -1,18 +1,19 @@
 import sqlite3
 import pandas as pd
+import re
 
 
 connection = sqlite3.connect('errepar-data.db')
 cursor = connection.cursor()
-query_servicios = open('search_most_used.sql', mode='r', encoding='utf-8')
-query_clientes = open('get_clients.sql', mode='r', encoding='utf-8')
+query_servicios = open('search_most_used.sql', mode='r', encoding='utf-8').read()
+query_clientes = open('get_clients.sql', mode='r', encoding='utf-8').read()
 
-cursor.execute(query_clientes.read())
+cursor.execute(query_clientes)
 clientes = list(map(lambda cliente: cliente[0], cursor.fetchall()))
 
 
 def get_client_n_most_used_services(client_code, n):
-    cursor.execute(query_servicios.read() % (client_code, n))
+    cursor.execute(query_servicios.format(client_code, n))
     headers = list(map(lambda x: x[0], cursor.description))
     results = cursor.fetchall()
     return headers, results
@@ -28,10 +29,10 @@ def clean_values(value):
         return 'None'
 
     if aux == 'N':
-        return 'False'
+        return '0'
 
     if aux == 'S':
-        return 'True'
+        return '1'
 
     if aux == 'PJ':
         return '1'
@@ -42,9 +43,44 @@ def clean_values(value):
     return aux
 
 
-header, datos = get_client_n_most_used_services(90218, 5)
-datos = list(map(lambda lista: list(map(lambda x: clean_values(str(x)), lista)), datos))
+def extract_years(text):
+    matches = re.search('[0-9]+', text)
 
-df = pd.DataFrame(data=datos, columns=header)
+    if matches is None:
+        return text
+
+    return matches.group(0)
 
 
+limitar_cantidad_clientes = True
+cantidad_clientes_buscar = 20
+
+n_max_servicios = 5
+factor_replicacion = 10
+
+contador = 0
+maximo = len(clientes)
+
+df = pd.DataFrame()
+for cliente in clientes:
+    header, datos = get_client_n_most_used_services(cliente, n_max_servicios)
+    datos = list(map(lambda lista: list(map(lambda x: clean_values(str(x)), lista)), datos))
+
+    df_aux = pd.DataFrame(data=datos, columns=header)
+    df_aux['TotalVisitas'] = pd.to_numeric(df_aux['CantidadVisitas']).sum()
+    df_aux['Proporcion'] = (pd.to_numeric(df_aux['CantidadVisitas']) / pd.to_numeric(df_aux['TotalVisitas'])).round(2)
+    df_aux['CantidadReplicaciones'] = (df_aux['Proporcion'] * factor_replicacion).round()
+
+    df = df.append(df_aux)
+
+    contador += 1
+    print('Procesando cliente {0}'.format(contador))
+
+    if limitar_cantidad_clientes and clientes.index(cliente) + 1 == cantidad_clientes_buscar:
+        break
+
+
+df['TasaEOL'] = df['TasaEOL'].apply(lambda x: extract_years(x))
+df['TasaIUS'] = df['TasaIUS'].apply(lambda x: extract_years(x))
+
+df.to_csv('dataset_intermedio.csv', index=False, sep=';')
--- search_most_used.sql
+++ search_most_used.sql
@@ -1,11 +1,20 @@
-select bd.clicod as CodigoCliente, bdc.FisJur as TipoPersona, bdc.age as Edad, bdc.tasaeol as TasaEOL, bdc.tasaius as TasaIUS, bdc.debaut as DebitoAutomatico, bdc.score as Score,
-       t.idTema as IdTema, s.idSubTemas as IdSubtema, b.idTypeBehaviour as IdBehaviour, sum(bd.frecuBehaviour) as CantidadVisitas
+select bd.clicod              as CodigoCliente,
+       bdc.FisJur             as TipoPersona,
+       bdc.age                as Edad,
+       bdc.tasaeol            as TasaEOL,
+       bdc.tasaius            as TasaIUS,
+       bdc.debaut             as DebitoAutomatico,
+       bdc.score              as Score,
+       t.idTema               as IdTema,
+       s.idSubTemas           as IdSubtema,
+       b.idTypeBehaviour      as IdBehaviour,
+       sum(bd.frecuBehaviour) as CantidadVisitas
 from behaviours_data as bd
-join behaviours_data_client as bdc on bd.clicod = bdc.cliCod
-join behaviours b on bd.NameOfBehaviour = b.nameOfBehaviour
-join subtemas s on bd.NameSubTema = s.nameSubtema
-join temas t on bd.NameTema = t.nameTema
-where bd.clicod = %s
+       join behaviours_data_client as bdc on bd.clicod = bdc.cliCod
+       join behaviours b on bd.NameOfBehaviour = b.nameOfBehaviour
+       join subtemas s on bd.NameSubTema = s.nameSubtema
+       join temas t on bd.NameTema = t.nameTema
+where bd.clicod = {0}
 group by bd.NameOfBehaviour
 order by cantidadVisitas desc
-limit %s
+limit {1}
