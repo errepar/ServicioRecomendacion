1884136352997fb662add9dbdf9e006d7cc79cac
Francisco <francisco.serrano372@gmail.com>
2019-01-31 01:02:31 -0300

    Inits model integration with REST API

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, jsonify
+from keras.models import load_model
+import numpy as np
 
 app = Flask(__name__)
+clasificador_tema = load_model('./dls/models/recomendador-errepar-tema_4/model.h5')
 
 
 @app.route('/')
 def hello_world():
-    return jsonify(mensaje='que onda bigote')
+    # Hacer que lleguen 10 parámetros (uno por cada feature del vector de entrada)
+    entrada = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    # Ranking
+    prediccion = clasificador_tema.predict(entrada)
+
+    return jsonify(prediccion=str(prediccion))
 
 
 if __name__ == '__main__':
     app.run()
+
---
+++ model.py
@@ -0,0 +1,3 @@
+from keras.models import load_model
+
+model = load_model('./dls/models/recomendador-errepar-tema_4/model.h5')
